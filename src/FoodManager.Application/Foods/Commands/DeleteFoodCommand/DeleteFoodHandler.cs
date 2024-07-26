73a7a47440660ccf36dc1dcde566bb7fc3b03eca
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2024-07-25 22:27:39 -0300

    create new command and queries from new methods

---
+++ src/FoodManager.Application/Foods/Commands/DeleteFoodCommand/DeleteFoodHandler.cs
@@ -0,0 +1,56 @@
+using FoodManager.API.Enums;
+using FoodManager.Application.Common.Exceptions;
+using FoodManager.Infrastructure.Database;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodManager.Application.Foods.Commands.DeleteFoodCommand;
+
+public class DeleteFoodHandler : IRequestHandler<DeleteFoodCommand, bool>
+{
+    private readonly DataBaseContext _context;
+
+    public DeleteFoodHandler(DataBaseContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> Handle(DeleteFoodCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var getFoodById = await _context
+                        .Foods
+                        .Where(x => !x.IsDeleted)
+                        .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (getFoodById is null)
+            {
+                throw new HttpResponseException
+                {
+                    Status = 404,
+                    Value = new
+                    {
+                        Code = CodeErrorEnum.NOT_FOUND_RESOURCE.ToString(),
+                        Message = "Comida não encontrada ou não existe",
+                    }
+                };
+            }
+
+            getFoodById.IsDeleted = true;
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            throw new HttpResponseException
+            {
+                Value = new
+                {
+                    Error = ex.Message,
+                }
+            };
+        }
+    }
+}
