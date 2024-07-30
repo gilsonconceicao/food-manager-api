b2be35ff6d235ba6510e631d0b7060e637c5aa9b
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2024-07-29 23:02:10 -0300

    configurations relationship

---
+++ src/FoodManager.Infrastructure/Database/Configurations/AddressConfiguration.cs
@@ -0,0 +1,16 @@
+
+using crud_products_api.src.Models;
+using FoodManager.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class AddressConfiguration : IEntityTypeConfiguration<Address>
+{
+    public void Configure(EntityTypeBuilder<Address> builder) 
+    { 
+        builder
+            .HasOne(x => x.Client)
+            .WithOne(x => x.Address)
+            .HasForeignKey<Client>(x => x.AddressId);
+    }
+}
---
+++ src/FoodManager.Infrastructure/Database/Configurations/ClientConfiguration.cs
@@ -0,0 +1,15 @@
+using crud_products_api.src.Models;
+using FoodManager.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class ClientConfiguration : IEntityTypeConfiguration<Client>
+{
+    public void Configure(EntityTypeBuilder<Client> builder) 
+    { 
+        builder
+            .HasOne(x => x.Address)
+            .WithOne(x => x.Client)
+            .HasForeignKey<Client>(x => x.AddressId);
+    }
+}
