02f83be1d6716740d163beea37df75b16b964f6a
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2024-09-13 17:25:01 -0300

    add last migrations update-relationships

---
+++ src/FoodManager.Infrastructure/Migrations/20240913175014_update-relationships.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updaterelationships : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FoodOrderRelation");
+
+            migrationBuilder.DropColumn(
+                name: "PreparationTime",
+                table: "Foods");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Foods",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    FoodId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    Discount = table.Column<decimal>(type: "numeric", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => new { x.FoodId, x.OrderId });
+                    table.ForeignKey(
+                        name: "FK_Items_Foods_FoodId",
+                        column: x => x.FoodId,
+                        principalTable: "Foods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Items_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Items_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_OrderId",
+                table: "Items",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_UserId",
+                table: "Items",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Foods");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PreparationTime",
+                table: "Foods",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "FoodOrderRelation",
+                columns: table => new
+                {
+                    FoodId = table.Column<Guid>(type: "uuid", nullable: false),
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodOrderRelation", x => new { x.FoodId, x.OrderId });
+                    table.ForeignKey(
+                        name: "FK_FoodOrderRelation_Foods_FoodId",
+                        column: x => x.FoodId,
+                        principalTable: "Foods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FoodOrderRelation_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FoodOrderRelation_OrderId",
+                table: "FoodOrderRelation",
+                column: "OrderId");
+        }
+    }
+}
