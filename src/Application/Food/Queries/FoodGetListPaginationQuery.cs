3de55f866b56f77c04b5500cdd2c24aa9d3b53e3
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2025-01-27 20:27:07 -0300

    filter by searchstring

--- src/Application/Food/Queries/FoodGetListPaginationQuery.cs
+++ src/Application/Food/Queries/FoodGetListPaginationQuery.cs
@@ -1,16 +1,19 @@
 
 
+using Application.Utils;
 using AutoMapper;
 using Domain.Extensions;
 using Domain.Models;
 using Infrastructure.Database;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+#nullable disable
 
 namespace Application.Foods.Queries.FoodGetListPaginationQuery
 {
     public class FoodGetListPaginationQuery : IRequest<ListDataResponse<List<Food>>>
     {
+        public string? SearchString { get; set; }
         public int Page { get; set; } = 0;
         public int Size { get; set; } = 5;
     }
@@ -28,6 +31,7 @@ namespace Application.Foods.Queries.FoodGetListPaginationQuery
 
         public async Task<ListDataResponse<List<Food>>> Handle(FoodGetListPaginationQuery request, CancellationToken cancellationToken)
         {
+            string searchString = request?.SearchString?.ToLower() ?? "";
             var page = request.Page;
             var size = request.Size;
 
@@ -35,6 +39,10 @@ namespace Application.Foods.Queries.FoodGetListPaginationQuery
                 .Include(x => x.Items)
                 .ThenInclude(x => x.Order)
                 .Where(x => !x.IsDeleted)
+                .Where(x => string.IsNullOrEmpty(searchString) || 
+                    x.Name.ToLower().Contains(searchString) ||
+                    x.Description.ToLower().Contains(searchString)
+                )
                 .OrderBy(c => c.Name);
 
             var totalCount = await queryData.CountAsync(cancellationToken);
