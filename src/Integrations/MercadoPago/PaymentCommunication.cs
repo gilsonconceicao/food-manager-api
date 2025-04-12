0db23ec421e115dc822149051f12767c34917d70
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2025-04-12 00:43:33 -0300

    improvements in webhook

--- src/Integrations/MercadoPago/PaymentCommunication.cs
+++ src/Integrations/MercadoPago/PaymentCommunication.cs
@@ -9,6 +9,7 @@ using MercadoPago.Client.Preference;
 using MercadoPago.Config;
 using MercadoPago.Resource.Preference;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Integrations.MercadoPago;
@@ -19,19 +20,22 @@ public class PaymentCommunication : IPaymentCommunication
     private readonly ICurrentUser _httpUserService;
     private readonly DataBaseContext _context;
     private readonly IMercadoPagoClient _mercadoPagoClient;
+     private readonly ILogger<PaymentCommunication> _logger;
 
 
     public PaymentCommunication(
         IOptions<MercadoPagoSettings> mercadoPagoSettings,
         ICurrentUser httpUserService,
         DataBaseContext context,
-        IMercadoPagoClient mercadoPagoClient
+        IMercadoPagoClient mercadoPagoClient,
+        ILogger<PaymentCommunication> logger
     )
     {
         _mercadoPagoSettings = mercadoPagoSettings.Value;
         _httpUserService = httpUserService;
         _context = context;
-        _mercadoPagoClient = mercadoPagoClient; 
+        _mercadoPagoClient = mercadoPagoClient;
+        _logger = logger;
     }
 
     public async Task<Preference> CreateCheckoutProAsync(
@@ -98,14 +102,21 @@ public class PaymentCommunication : IPaymentCommunication
             .Where(o => o.Status == OrderStatus.AwaitingPayment && !string.IsNullOrEmpty(o.PaymentId))
             .ToListAsync();
 
+        if (!ordersPending.Any())
+        {
+            _logger.LogInformation("Nenhum pedido pendente com PaymentId encontrado.");
+            return;
+        }
+
         foreach (var order in ordersPending)
         {
-            var status = await _mercadoPagoClient.GetPaymentStatusAsync(order.PaymentId);
-            
+            var status = await _mercadoPagoClient.GetPaymentStatusAsync(order.PaymentId!);
+
             if (status == "approved")
             {
                 order.Status = OrderStatus.Paid;
                 _context.Orders.Update(order);
+                _logger.LogInformation($"O pedido #{order.RequestNumber} alterou o status para pago! ID: {order.Id}");
             }
         }
 
@@ -113,43 +124,43 @@ public class PaymentCommunication : IPaymentCommunication
     }
 
     public async Task ProcessPaymentWebhookAsync(string paymentId)
-{
-    try
     {
-        var accessToken = _mercadoPagoSettings.AccessToken;
-        var response = await new HttpClient().GetAsync(
-            $"https://api.mercadopago.com/v1/payments/{paymentId}?access_token={accessToken}");
-
-        response.EnsureSuccessStatusCode();
-
-        var content = await response.Content.ReadAsStringAsync();
-        var json = JsonDocument.Parse(content).RootElement;
-
-        var status = json.GetProperty("status").GetString();
-        var externalReference = json.GetProperty("external_reference").GetString(); 
-
-        if (string.IsNullOrEmpty(externalReference))
-            return;
-
-        var userIdStr = externalReference.Split('-').ElementAtOrDefault(1);
-        if (!Guid.TryParse(userIdStr, out var userId))
-            return;
-
-        var order = await _context.Orders
-            .FirstOrDefaultAsync(x => x.UserId == userId && x.Status == OrderStatus.AwaitingPayment);
-
-        if (order == null) return;
-
-        if (status == "approved")
+        try
         {
-            order.Status = OrderStatus.Paid;
-            _context.Orders.Update(order);
-            await _context.SaveChangesAsync();
+            var accessToken = _mercadoPagoSettings.AccessToken;
+            var response = await new HttpClient().GetAsync(
+                $"https://api.mercadopago.com/v1/payments/{paymentId}?access_token={accessToken}");
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var json = JsonDocument.Parse(content).RootElement;
+
+            var status = json.GetProperty("status").GetString();
+            var externalReference = json.GetProperty("external_reference").GetString();
+
+            if (string.IsNullOrEmpty(externalReference))
+                return;
+
+            var userIdStr = externalReference.Split('-').ElementAtOrDefault(1);
+            if (!Guid.TryParse(userIdStr, out var userId))
+                return;
+
+            var order = await _context.Orders
+                .FirstOrDefaultAsync(x => x.UserId == userId && x.Status == OrderStatus.AwaitingPayment);
+
+            if (order == null) return;
+
+            if (status == "approved")
+            {
+                order.Status = OrderStatus.Paid;
+                _context.Orders.Update(order);
+                await _context.SaveChangesAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro no webhook: {ex.Message}");
         }
     }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Erro no webhook: {ex.Message}");
-    }
-}
 }
