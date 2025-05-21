9e186b535e7ae720b7b2915976bae4403c49be30
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2025-05-20 22:32:41 -0300

    fix: return url food

--- src/Application/Food/Dtos/FoodItemsDto.cs
+++ src/Application/Food/Dtos/FoodItemsDto.cs
@@ -5,6 +5,7 @@ namespace Application.Foods.Queries.GetAllWithPaginationFoodQuery
     public class FoodItemsDto
     {
         public string Name { get; set; }
+        public string Url { get; set; }
         public decimal Price { get; set; }
         public string CategoryDisplay { get; set; }
         public FoodCategoryEnum? Category { get; set; }
